29bedb2e378c27048778e32e6d3cf2bf1d15ddcd
kunal <kunalk8728@gmail.com>
2026-08-21 15:42:26 +0530

    initialzing the endpoints

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,42 @@
 import express from "express";
 
+
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.json({
-    message: "Server is jumping and running!"
-  });
+app.post("/api/v1/signup", (req, res) => {
+  const {username, email, password} = req.body;
 });
+
+
+app.post("/api/v1/signin", (req, res) => {
+  
+});
+
+
+app.post("/api/v1/content", (req, res) => {
+  
+});
+
+
+app.get("/api/v1/content", (req, res) => {
+  
+});
+
+
+app.delete("/api/v1/content", (req, res) => {
+  
+});
+
+
+
+app.get("/api/v1/brain/ :shareLink", (req, res) => {
+  
+});
+
+
+
 
 app.listen(3000, () => {
   console.log("Server running on http://localhost:3000");
