63bdb6f6d21ae5c00f0f887dd259cd49b98ada4d
kunal <kunalk8728@gmail.com>
2026-08-22 10:59:40 +0530

    added the signup and also connect mongobd

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,18 @@
-import express from "express";
-import { signupSchema } from "";
+import dns from "dns";
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
+import dotenv from "dotenv";
+dotenv.config();
+
+
+import express from "express";
+import mongoose from "mongoose";
+import bcrypt from "bcrypt"
+import { UserModel } from "./db.js";
+
+const MONGO_URL  = process.env.MONGO_URL as string;
+
+mongoose.connect(MONGO_URL)
 
 
 
@@ -8,10 +20,23 @@ const app = express();
 
 app.use(express.json());
 
-app.post("/api/v1/signup", signupSchema, (req, res) => {
+app.post("/api/v1/signup", async (req, res) => {
   const {username, email, password} = req.body;
-});
 
+  const HashPassword = await bcrypt.hash(password, 6);
+
+  await  UserModel.create({
+    username: username,
+    email: email,
+    password: HashPassword,
+  })
+
+  res.json({
+    message: "Profile has been created"
+  })
+
+});
+ 
 
 app.post("/api/v1/signin", (req, res) => {
   
