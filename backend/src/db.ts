f2a2aa6cf9298ce445621ea6f543cca90cea0132
kunal <kunalk8728@gmail.com>
2026-08-22 18:37:13 +0530

    included the ContentSchema

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,4 +1,4 @@
-import { model, Schema } from "mongoose";
+import mongoose, { model, Schema } from "mongoose";
 
 const UserSchema = new Schema({
     username: { type: String, unique: true, required: true },
@@ -7,3 +7,12 @@ const UserSchema = new Schema({
 });
 
 export const UserModel = model("User", UserSchema);
+
+const ContentSchema = new Schema({
+    Title: String,
+    Link:  String,
+    tags: [{type: mongoose.Types.ObjectId, ref: "Tag"}], // it will be in object form in bunch
+    userId :{type: mongoose.Types.ObjectId, ref: "User", require: true}
+})
+
+export const ContentModel = model("Content", ContentSchema)
