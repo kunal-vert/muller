b077ab0949070999e69ba6863735d539ba0301c6
kunal <kunalk8728@gmail.com>
2026-08-25 13:56:13 +0530

    pesuodo test

--- muller-frontend/src/App.tsx
+++ muller-frontend/src/App.tsx
@@ -1,8 +1,13 @@
+import { Button } from "./components/UI/Button"
+import { PlusIcon } from "./Icons/PlusIcon"
 
-const App = () =>{
-  return <div className="">
-     
-  </div>
+const App = () => {
+  return (
+    <>
+      <Button variant="primary" text="share" size="sm" onClick={() => { }} />
+      <Button startIcon={<PlusIcon size="lg" />} variant="secondary" text="Add content" size="md" onClick={() => { }} />
+      <Button startIcon={<PlusIcon size="md" />} variant="secondary" text="Add content" size="lg" onClick={() => { }} />
+    </>)
 }
 
 export default App
