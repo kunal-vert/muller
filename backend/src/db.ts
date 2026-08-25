3aacd28bf6fb0e3194817cfebcacfe34376ca136
kunal <kunalk8728@gmail.com>
2026-08-25 17:51:43 +0530

    created new table for share Link

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -16,3 +16,10 @@ const ContentSchema = new Schema({
 })
 
 export const ContentModel = model("Content", ContentSchema)
+
+const LinkSchema = new Schema({
+    Hash: String,
+    userId :{type: mongoose.Types.ObjectId, ref: "User", require: true, unique: true }
+})
+
+export const LinkModel  = model("Links", LinkSchema)
