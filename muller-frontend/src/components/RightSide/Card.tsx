b056984fc0f16ac416ed228f70e55c5bc4ac1761
kunal <kunalk8728@gmail.com>
2026-08-26 18:21:08 +0530

    custmizing the buttons

--- muller-frontend/src/components/RightSide/Card.tsx
+++ muller-frontend/src/components/RightSide/Card.tsx
@@ -1,9 +1,22 @@
+import ShareIcon from "../../Icons/ShareIcon"
 
 
 const Card = () => {
   return (
-    <div className="bg-red-500">
-        MainCotent bruhh
+    <div className="p-4 bg-black rounded-md shadow-md outline-slate-200 max-w-72 border-gray-900 ">
+      <div className="flex justify-between items-center">
+        <div className="text-gray-500 pr-2">
+          <ShareIcon size="md" />
+        </div>
+        Projects Ideas
+        <div className="flex items-center gap-6 text-gray-500">
+          <div className="pr-2">
+            <ShareIcon size="md" />
+          </div>
+          <ShareIcon size="md" />
+        </div>
+      </div>
+
     </div>
   )
 }
