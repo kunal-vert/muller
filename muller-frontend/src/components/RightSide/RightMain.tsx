76cf47846438a6d43f1652420315cbda71043fd0
kunal <kunalk8728@gmail.com>
2026-08-26 18:56:16 +0530

    resolved the overflow content

--- muller-frontend/src/components/RightSide/RightMain.tsx
+++ muller-frontend/src/components/RightSide/RightMain.tsx
@@ -1,12 +1,22 @@
 import { PlusIcon } from "../../Icons/PlusIcon"
+import ShareIcon from "../../Icons/ShareIcon"
 import { Button } from "../Button"
 import Card from "./Card"
 
 
 const RightMain = () => {
   return (
-    <div className="w-full h-full p-6 border-x-2 border-gray-950">
-        <Button startIcon={<PlusIcon size="lg" />} variant="primary" text="Add Content" size="lg" onClick={() => { }}/>
+    <div className="w-full h-full p-6 overflow-y-auto ">
+      <div className="flex justify-end items-center">
+        <ShareIcon size="lg" />
+        <ShareIcon size="lg" />
+
+      </div>
+      <div className="py-11 flex flex-wrap gap-3">
+        <Card />
+        <Card />
+      
+      </div>
     </div>
   )
 }
