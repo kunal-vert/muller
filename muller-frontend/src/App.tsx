3cbb51aadba8f2dadf8061a4666f80a8a6a26956
kunal <kunalk8728@gmail.com>
2026-08-27 21:11:46 +0530

    add the components

--- muller-frontend/src/App.tsx
+++ muller-frontend/src/App.tsx
@@ -1,27 +1,25 @@
-import { Button } from "./components/Button"
+import { useState } from "react"
 import SideBar from "./components/LeftSide/SideBar"
 import RightMain from "./components/RightSide/RightMain"
-import { PlusIcon } from "./Icons/PlusIcon"
-import ShareIcon from "./Icons/ShareIcon"
-import AddCloseModel from "./UI/AddCloseModel.tsx"
+import AddCloseModel from "./UI/AddCloseModel"
+
 
 const App = () => {
+  const [ModalOpen, setModalOpen] = useState<boolean>(true)
+
   return (
-    // <div className="flex items-center justify-center gap-3 bg-neutral-900  ">
-    //   <Button variant="secondary" text="share" size="sm" onClick={() => { }} />
-    //   <Button startIcon={<ShareIcon size="md" />} variant="secondary" text="Share muller" size="md" onClick={() => { }} />
-    //   <Button startIcon={<PlusIcon size="lg" />} variant="primary" text="Add Content" size="lg" onClick={() => { }} />
-    // </div>
+    <div>
+      <AddCloseModel open={ModalOpen} onClose={() => {
+        setModalOpen(false)
+      }} />
 
-    <div className="">
-      <AddCloseModel open={true}/>
-      <div className="grid grid-cols-1 md:grid-cols-[250px_1fr] min-h-screen w-full bg-neutral-950 text-white">
-    
-      <SideBar />
-      <RightMain />
+     
+       
       
-
-    </div>
+      <div className="grid grid-cols-1 md:grid-cols-[250px_1fr] min-h-screen w-full bg-neutral-950 text-white">
+        <SideBar functionButton={setModalOpen} />
+        <RightMain functionButton={setModalOpen} />
+      </div>
     </div>
   )
 }
