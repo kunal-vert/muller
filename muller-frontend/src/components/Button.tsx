9c7a99f9c83863778fa6b1a8dab197913b593cee
kunal <kunalk8728@gmail.com>
2026-08-27 19:38:48 +0530

    Add the onclick, cursor-p

--- muller-frontend/src/components/Button.tsx
+++ muller-frontend/src/components/Button.tsx
@@ -7,17 +7,17 @@ export interface ButtonProps {
   text: string;
   startIcon?: any; //optinal
   endIcon?: any;//optional
-  onClick: () => void;
+  onClick?: () => void;
 }
 const sizeStyles = {
   "sm": "px-3 py-1.5 text-sm font-medium",
-  "md": "px-4 py-2 text-base font-semibold",
+  "md": " px-4 py-2 text-base font-semibold",
   "lg": "px-5 py-3 text-lg font-bold"
 }
 
 const flexStyles = "flex items-center justify-center gap-2 "
 
-const defaultStyles = "rounded-2xl transition-all duration-200 ease-in-out active:scale-95 "
+const defaultStyles = "rounded-2xl transition-all duration-200 ease-in-out active:scale-95 cursor-pointer "
 
 const ColorVariant = {
   "primary": "bg-purple-600  text-gray-400",
@@ -25,7 +25,7 @@ const ColorVariant = {
 }
 
 export const Button = ({ variant, size, text, startIcon, endIcon, onClick }: ButtonProps) => {
-  return <button className={`${ColorVariant[variant]} ${defaultStyles} ${sizeStyles[size]} ${flexStyles} `}  >{startIcon ? <div >{startIcon}</div> : null} {text} {endIcon}</button>
+  return <button className={`${ColorVariant[variant]} ${defaultStyles} ${sizeStyles[size]} ${flexStyles} ${onClick} `}  >{startIcon ? <div >{startIcon}</div> : null} {text} {endIcon}</button>
 }
 
 
