033049cccd5e5ccc7a03a14dbb4c66888f800da1
kunal <kunalk8728@gmail.com>
2026-08-27 21:09:12 +0530

    fix some error

--- muller-frontend/src/components/RightSide/RightMain.tsx
+++ muller-frontend/src/components/RightSide/RightMain.tsx
@@ -3,19 +3,22 @@ import ShareIcon from "../../Icons/ShareIcon"
 import { Button } from "../Button"
 import Card from "./Card"
 
+// ADDED: TypeScript interface to define the prop
+interface RightMainProps {
+  functionButton: (value: boolean) => void;
+}
 
-const RightMain = () => {
+// FIXED: Applied the interface to the props
+const RightMain = ({ functionButton }: RightMainProps) => {
   return (
     <div className="w-full h-full p-6 overflow-y-auto ">
-      <div className="flex justify-end items-center gap-4">
-        <Button  variant="primary" size="lg" text="Add Content" startIcon={<PlusIcon size="lg" />}  />
+      <div className="flex justify-end items-center gap-4"> 
+        <Button onClick={() => functionButton(true)} variant="primary" size="lg" text="Add Content" startIcon={<PlusIcon size="lg" />}  />
         <Button variant="secondary" size="lg" text="Share" startIcon={<ShareIcon size="lg" />} />
-
       </div>
       <div className="py-11 flex flex-wrap gap-3">
         <Card />
         <Card />
-
       </div>
     </div>
   )
