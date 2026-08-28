4b0889b7328eb6118afe608098f2b8491040e9dd
kunal <kunalk8728@gmail.com>
2026-08-28 12:30:38 +0530

    need ya the interface

--- muller-frontend/src/components/RightSide/ModelBox.tsx
+++ muller-frontend/src/components/RightSide/ModelBox.tsx
@@ -1,8 +1,14 @@
-export function InputBox({ onChange, placeholder }: { onChange: () => void, placeholder: string }) {
+
+interface InputArg {
+  placeholder: string;
+  ref: any
+}
+
+export function InputBox({ placeholder, ref }: InputArg) {
   return (
     <div>
-      <input placeholder={placeholder} type={"text"} className=" w-full py-2 px-4 
-      text-center bg-gray-800 text-white border border-gray-700 rounded-lg focus:outline-none  focus:border-purple-500 transition-colors " onChange={onChange}></input>
+      <input ref={ref} placeholder={placeholder} type={"text"} className=" w-full py-2 px-3
+       bg-gray-800 text-white border border-gray-700 rounded-lg focus:outline-none  focus:border-purple-500 transition-colors " ></input>
     </div>
   )
 }
