63468ad54ab984a1bf9fcad3f3224163014dc5a6
kunal <kunalk8728@gmail.com>
2026-08-28 11:28:38 +0530

    install the react router

--- muller-frontend/src/App.tsx
+++ muller-frontend/src/App.tsx
@@ -1,26 +1,17 @@
-import { useState } from "react"
-import SideBar from "./components/LeftSide/SideBar"
-import RightMain from "./components/RightSide/RightMain"
-import AddCloseModel from "./UI/AddCloseModel"
-
+import React from 'react'
+import Dashboard from './pages/Dashboard'
+import Signup from './pages/Signup'
+import Signin from './pages/Signin'
+import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
-  const [ModalOpen, setModalOpen] = useState<boolean>(true)
-
   return (
-    <div>
-      <AddCloseModel open={ModalOpen} onClose={() => {
-        setModalOpen(false)
-      }} />
-
-     nn
-       
+    <Routes>
+      <Route path='/signup' element={<Signup/>} />
+      <Route path='/signin' element={<Signin/>} />
+      <Route path='/dashboard' element={<Dashboard/>} />
       
-      <div className="grid grid-cols-1 md:grid-cols-[250px_1fr] min-h-screen w-full bg-neutral-950 text-white">
-        <SideBar functionButton={setModalOpen} />
-        <RightMain functionButton={setModalOpen} />
-      </div>
-    </div>
+    </Routes>
   )
 }
 
